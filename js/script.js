e7bf7f46920c53719cf85edfa1420c92082859f1
Gustavo Dal Canton <gustavo.dalcanton19@gmail.com>
2025-04-01 09:47:25 -0300

    mostrar mensagens ao usuário caso tente adicionar operadores em lugares inadequados

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ function addSimbolo(x) {
     let aux = document.getElementById("display").value.slice(-1);
     if(aux || !isNaN(x) || x=="("){
     if (!isNaN(x) || x=="(" || aux == ")" || !isNaN(Number(aux))) {
+        document.getElementById("mensagem").innerHTML = "&nbsp";
         soma += x;
         if (x == "/100*") {
             document.getElementById("display").value += "%";
@@ -17,6 +18,7 @@ function addSimbolo(x) {
             document.getElementById("display").value += x;
         }
     } else {
+        document.getElementById("mensagem").innerText = "Não se pode adicionar um operador ao lado de outro!";
         soma = (soma.slice(0, -1)) + x;
         if (x == "/100*") {
             x = "%";
@@ -30,15 +32,20 @@ function addSimbolo(x) {
         document.getElementById("display").value = ((document.getElementById("display").value).slice(0, -1)) + x;
 
     }
+} else{
+    document.getElementById("mensagem").innerText = "Não se pode adicionar um operador no início!";
+
 }
 }
 
 function limpar() {
+    document.getElementById("mensagem").innerHTML = "&nbsp";
     document.getElementById("display").value = (document.getElementById("display").value).slice(0, -1);
     soma = soma.slice(0, -1);
 }
 
 function calcular() {
+    document.getElementById("mensagem").innerHTML = "&nbsp";
     if(!isNaN(eval(soma))){
     document.getElementById("display").value = (eval(soma) * 100) / 100;
     soma = ((eval(soma) * 100) / 100).toString();
